4cb343c33d245aa332e05eb068846e82e7eed4ae
Cheryl Bao <cheryl.bao@MacBook-Air-430.local>
2026-07-15 20:46:25 -0600

    Add a second conv layer (32->64) to the first block of SeasonCNN

--- CNN-models/model.py
+++ CNN-models/model.py
@@ -9,7 +9,10 @@ class SeasonCNN(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2),  # 224 -> 112
